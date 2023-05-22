98092418251a684bc363a09fb7766872591e876b
Errormacr <tulupov.valeriy@mail.ru>
2023-05-22 16:21:28 +0300

    Litlle fix

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from "./views/main_window";
-import User from "./views/personal_data_main_win";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
