ed3e00b36f8b43bc18d9fa69b4d33bd393ab2a5f
Валерий Тулупов <v.tulupov@synaptik.network>
2024-12-06 14:57:11 +0300

    Updated server subproject commit, added display flex and align-items center to CSS, and modified GamePage.tsx with various changes to imports, interfaces, and component rendering.

--- src/views/GamePage.tsx
+++ src/views/GamePage.tsx
@@ -12,41 +12,43 @@ import { ApiService } from '../services/api.services';
 import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
+import { GrUpdate } from 'react-icons/gr';
 interface Game {
-  appid: number;
-  last_launch_time: string;
-  playtime: number;
-  gameName: string;
-  all: number;
-  gained: number;
-  percent: number;
+    appid : number;
+    last_launch_time : string;
+    playtime : number;
+    gameName : string;
+    all : number;
+    gained : number;
+    percent : number;
 }
 
-const GamePage: React.FC = () => {
+const GamePage : React.FC = () => {
   const navigate = useNavigate();
 
-  const { appid, backWindow } = useParams<{
-    appid: string;
-    backWindow: string;
-  }>();
+  const { appid, backWindow } = useParams < {
+        appid: string;
+        backWindow: string;
+    } >();
 
-  const [game, setGame] = useState<Game>({
-    appid: 0,
-    last_launch_time: '',
-    playtime: 0,
-    gameName: '',
-    all: 0,
-    gained: 0,
-    percent: 0
-  });
-  const [loaded, setLoaded] = useState(false);
-  const [tableOrBox, setTableOrBox] = useState(true);
+  const [game,
+    setGame] = useState < Game >({
+      appid: 0,
+      last_launch_time: '',
+      playtime: 0,
+      gameName: '',
+      all: 0,
+      gained: 0,
+      percent: 0
+    });
+  const [loaded,
+    setLoaded] = useState(false);
+  const [tableOrBox,
+    setTableOrBox] = useState(true);
   const { t } = useTranslation();
   const renderComponent = async () => {
     const dataSteamId = localStorage.getItem('steamId');
-    const gameData = await ApiService.get<gameDataWithAch>(
-      `user/${dataSteamId}/game/${appid}/data?language=${i18n.language}&achievements=false`
-    );
+    const gameData = await ApiService.get < gameDataWithAch >(`user/${dataSteamId}/game/${appid}/data?language=${i18n.language}&achievements=false`);
     const newGameData = {
       appid: +appid,
       last_launch_time: gameData.userDatas[0].lastLaunchTime,
@@ -61,12 +63,8 @@ const GamePage: React.FC = () => {
 
   const fetchUpdatedGameData = async () => {
     const steamId = localStorage.getItem('steamId');
-    const { unlockedCount: gained } = await ApiService.get<GameDataRow>(
-      `steam-api/all-user-ach-data/${steamId}/game/${game.appid}?lang=${i18n.language}`
-    );
-    toast.success(
-      `${t('GameUpdateSuccess')}\n${t('Gained')} ${gained - game.gained}`
-    );
+    const { unlockedCount: gained } = await ApiService.get < GameDataRow >(`steam-api/all-user-ach-data/${steamId}/game/${game.appid}?lang=${i18n.language}`);
+    toast.success(`${t('GameUpdateSuccess')}\n${t('Gained')} ${gained - game.gained}`);
     renderComponent();
   };
 
@@ -84,64 +82,56 @@ const GamePage: React.FC = () => {
   }, []);
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <div>
-        <ToastContainer />
-        <ScrollToTopButton />
-        <FaArrowLeft
-                className="button-icon return" onClick={() => {
-                  if (backWindow === 'main') {
-                    navigate('/');
-                  } else {
-                    navigate('/Games');
-                  }
-                }}
-                id="return"/>
-        <div className="label-container">
-          <label className="game-label">{game.gameName}</label>
-        </div>
-        <div className="label-container preview-container">
-          <img
-            src={`https://steamcdn-a.akamaihd.net/steam/apps/${appid}/header.jpg`}
-            className="preview-img"
-            alt={game.gameName}
-          />
-          <ProgressRad
-            data-progress={`${game.percent}`}
-            SizeVnu={'9rem'}
-            SizeVne={'10rem'}
-          />
-          <label title={t('GainedFromAll')} className="gain-nongain">
-            {game.gained}/{game.all}
-          </label>
-        </div>
-        <div className="switchTable">
-          <GameButton
-            text={t('UpdateGame')}
-            onClick={fetchUpdatedGameData}
-            id="updateGame"
-          />
-          <GameButton
-            id=""
-            text={t('SwitchTable')}
-            onClick={() => {
-              setTableOrBox(!tableOrBox);
-              localStorage.setItem('boxView', String(!tableOrBox));
-            }}
-          />
-        </div>
-        <div className="table-container">
-          {loaded &&
-            (tableOrBox
-              ? (
-              <Table appid={+appid} all={false} />
-                )
-              : (
-              <AchBox appid={+appid} all={false} />
-                ))}
-        </div>
-      </div>
-    </I18nextProvider>
+        <I18nextProvider i18n={i18n}>
+            <div>
+                <ToastContainer/>
+                <ScrollToTopButton/>
+                <FaArrowLeft
+                    className="button-icon return"
+                    onClick={() => {
+                      if (backWindow === 'main') {
+                        navigate('/');
+                      } else {
+                        navigate('/Games');
+                      }
+                    }}
+                    id="return"/>
+                <div className="label-container">
+                    <label className="game-label">{game.gameName}</label>
+                </div>
+                <div className="label-container preview-container">
+                    <img
+                        src={`https://steamcdn-a.akamaihd.net/steam/apps/${appid}/header.jpg`}
+                        className="preview-img"
+                        alt={game.gameName}/>
+                    <ProgressRad
+                        data-progress={`${game.percent}`}
+                        SizeVnu={'9rem'}
+                        SizeVne={'10rem'}/>
+                    <label title={t('GainedFromAll')} className="gain-nongain">
+                        {game.gained}/{game.all}
+                    </label>
+                </div>
+                <div className="switchTable">
+                    <GrUpdate
+                        id=""
+                        className="button-icon update-button"
+                        onClick={fetchUpdatedGameData}/>
+                    <GameButton
+                        id=""
+                        text={t('SwitchTable')}
+                        onClick={() => {
+                          setTableOrBox(!tableOrBox);
+                          localStorage.setItem('boxView', String(!tableOrBox));
+                        }}/>
+                </div>
+                <div className="table-container">
+                    {loaded && (tableOrBox
+                      ? (<Table appid={+appid} all={false}/>)
+                      : (<AchBox appid={+appid} all={false}/>))}
+                </div>
+            </div>
+        </I18nextProvider>
   );
 };
 
