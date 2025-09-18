fe4308d52316d4b1dcfcb8eda0dcfb9fba13a610
errormacro <tulupov@synaptik.ru>
2025-09-18 13:24:52 +0300

    fix ach keys

--- src/features/AchievementList.tsx
+++ src/features/AchievementList.tsx
@@ -17,7 +17,7 @@ export const AchievementList: React.FC<AchievementListProps> = ({ achievements, 
       <div className="AchCont">
         {achievements.map((achievement, index, arr) => (
           <div
-            key={`${achievement.displayName}`}
+            key={`${achievement.displayName}-${achievement.game.gamename}`}
             ref={index === arr.length - 1 ? lastAchievementRef : undefined}
             className={`achievement-item ${newAchievements.includes(index) ? 'new-achievement' : ''}`}
           >
